aeb70f1ae573b7fc2b8e277b32f7da6f260b42ad
Bunescu Mihai <bunescumihai1958@gmail.com>
2022-09-17 22:24:18 +0300

    Third commit

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,5 +2,6 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("New line to test");
+        System.out.println("Second new line to test");
     }
 }
